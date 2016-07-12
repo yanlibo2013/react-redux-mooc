94c9e2d8c787e3c54487200ba7f3e231d5f1f663
yanlibo <yaniibo2010@163.com>
2016-07-12 21:26:43 +0800

    add home component

---
+++ app/component/home.jsx
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+/*
+ redux 相关
+ */
+import { connect } from 'react-redux';
+
+class About extends Component {
+    render() {
+        return (
+            <div className="about">
+                <h1>about</h1>
+            </div>
+        );
+    }
+};
+
+
+export default About;
